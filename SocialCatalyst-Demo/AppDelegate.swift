c3bb67a888d906e33805dbce0d79641a5be2857d
Михаил Луцкий <mikhail@lwts.ru>
2019-08-11 20:13:00 +0300

    Demo project

---
+++ SocialCatalyst-Demo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SocialCatalyst-Demo
+//
+//  Created by Михаил Луцкий on 11/08/2019.
+//  Copyright © 2019 Mikhail Lutskii. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
